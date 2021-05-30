465f54db624a6795f2aaa18aed643d98eb357a22
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-30 11:32:02 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		int mn = INT_MAX;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			// find the smallest number, because this number will not be deleted
+			mn = min(mn, a[i]);
+		}
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (a[i] != mn) {
+				// if the current number is not the smallest, then this will be deleted
+				cnt++;
+			}
+		}
+		cout << cnt << '\n';
+	}
+	return 0;
+}
